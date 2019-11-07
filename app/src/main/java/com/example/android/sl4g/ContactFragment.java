38092067cb7cdefb3d6ba4bc2d757e07fb37a750
Ookie Ebron <ookieebron@gmail.com>
2019-11-07 14:06:07 -0500

    --Need to make sure up navigation returns to specific page on viewpager --Email functionality now fixed on Contact/Booking page --Need to update text on contact page and add in my email instead of group --Need save instance state --clean up landscape layout

--- app/src/main/java/com/example/android/sl4g/ContactFragment.java
+++ app/src/main/java/com/example/android/sl4g/ContactFragment.java
@@ -1,12 +1,18 @@
 package com.example.android.sl4g;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.Objects;
 
 
 /**
@@ -14,6 +20,8 @@ import android.widget.EditText;
  */
 public class ContactFragment extends Fragment {
 
+    EditText nameResponse, feedbackResponse;
+    public static String[] sl4gEmail = {"switchinlanes4god@gmail.com"};
 
     public ContactFragment() {
         // Required empty public constructor
@@ -27,10 +35,42 @@ public class ContactFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_booking, container, false);
     }
 
-    // TODO: 4/6/2018 REFERENCE ALL BUTTONS ANS TEXTBOXES - FURTHER EXTRACT USER RESPONSES FROM THESE VARIABLES
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
-//    EditText nameResponse = getActivity().findViewById(R.id.enterName);
-//    EditText feebackResponse = getActivity().findViewById(R.id.enterFeedback);
+        //Reference the Views
+        nameResponse = getView().findViewById(R.id.enterName);
+        feedbackResponse = getView().findViewById(R.id.enterFeedback);
+        Button prayerButton = getView().findViewById(R.id.prayerButton);
+
+        prayerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Get the values AFTER user clicks button with GETTEXT()!!!
+                String name = nameResponse.getText().toString();
+                String text = feedbackResponse.getText().toString();
+                composeEmail(sl4gEmail, name, text);
+            }
+        });
+    }
+
+
+    public void composeEmail(String[] addresses, String subject, String text) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+
+        //set package:
+//        if (intent.resolveActivity(getPackageManager()) != null) {
+//            startActivity(intent);
+//        }
+
+        startActivity(intent);
+    }
 
 
 }
